858e26db53956366ebf1c4d4d3338a5c47a41554
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-08 20:11:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,12 +15,12 @@ block_return_values=[]
 while array.select {|word| word.length>4}
 block_return_values << yield (array[i])
 i = i + 1
-end 
+end
   if array.include?(false)
     false
-  else 
+  else
    true
-   end 
+   end
 end
 
 
