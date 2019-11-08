7ac3270fcf41e792fbab681284e7812d42d47475
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-08 20:38:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
       array.find do |type|
       cheese_types.include?(type)
-    end 
+    end
   end
-  
+
   find_the_cheese(snacks)
