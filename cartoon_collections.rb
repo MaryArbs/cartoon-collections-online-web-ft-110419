da0d2b99589bbbd3c1594bcb87f6144319faaedd
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-08 20:04:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ array.map { |name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
-  array.select {|word| word.length>4}
+  array.includee? {|word| word.length>4}
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
