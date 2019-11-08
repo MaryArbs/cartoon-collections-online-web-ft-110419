381b06bffd50b98dad30a2391c8b2d2cd4f33a60
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-08 18:31:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,7 @@ def summon_captain_planet(array)
 array.map { |name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
